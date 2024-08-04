c5d177158d9396656456b2940da7294f9d579955
Ritika Agrawal <ritikaagrawal079@gmail.com>
2024-08-04 22:28:06 +0530

    Gemini Clone

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Sidebar from './components/Sidebar/Sidebar'
+import Main from './components/Main/Main.jsx'
+
+const App = () => {
+  return (
+    <div id="root">
+      <Sidebar/>
+      <Main/>
+    </div>
+  )
+}
+
+export default App
---
+++ src/assets/assests.js
@@ -0,0 +1,33 @@
+import history_icon from "./history_icon.jpg";
+import menu_icon from "./menu_icon.jpg";
+import plus_icon from "./plus_icon.jpg";
+import bulb_icon from "./bulb.jpg";
+import code_icon from "./code_icon.jpg";
+import compass_icon from "./compass_icon.jpg";
+import gallery_icon from "./gallery_icon.jpg";
+import message_icon from "./message_icon.jpg";
+import mic_icon from "./mic_icon.jpg";
+import question_icon from "./question_icon.jpg";
+import send_icon from "./send_icon.jpg";
+import setting_icon from "./setting_icon.jpg";
+import user_icon from "./user_icon.jpg";
+import youtube_icon from "./youtube_icon.jpg";
+import gemini_icon from "./gemini_icon.jpg";
+
+export const assets = {
+  history_icon,
+  menu_icon,
+  plus_icon,
+  bulb_icon,
+  youtube_icon,
+  user_icon,
+  setting_icon,
+  send_icon,
+  question_icon,
+  mic_icon,
+  message_icon,
+  gallery_icon,
+  compass_icon,
+  code_icon,
+  gemini_icon,
+};
